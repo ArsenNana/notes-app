3cce3ae56aad1ac6e3984d9347e493776c262dc4
NanaHans <arsenmajeste@gmail.com>
2022-05-11 22:10:55 +0200

    new model added

--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -2,7 +2,19 @@ export class User {
     id!: number;
     username!: string;
     password!: string;
-    firtsName!: string;
-    lansName!: string;
+    passwordConfirmed!: string;
+    firstName!: string;
+    lanstName!: string;
     token!: string;
+
+    constructor(id: number, username: string, password: string, firstName: string, lanstName: string) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lanstName = lanstName;
+        this.password = password;
+
+    }
+
+    getFirstName(): string { return this.firstName; }
 }
