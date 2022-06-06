a40856a5805a579015bed1f1109e7f8ad39c7a4d
NanaHans <arsenmajeste@gmail.com>
2022-06-06 09:18:48 +0200

    navigate to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NoteDetailsComponent } from './pages/note-details/note-details.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: '', component: MainLayoutComponent, children: [
       { path: '', component: LoginComponent },
       { path: 'register', component: UserRegisterComponent },
+      { path: 'home', component: HomeComponent },
       { path: 'noteList', component: NotesListComponent },
       { path: 'new', component: NoteDetailsComponent },
       { path: ':id', component: NoteDetailsComponent }
